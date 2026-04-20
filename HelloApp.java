9087ecef612dd905138c96b18c126c598e3b2846
Charanpreet Kaur <ccharnpreet10@gmail.com>
2026-04-20 09:26:05 +0530

    uc7 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,25 +1,13 @@
 public class HelloApp {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        String greeting;
+        if (args.length == 0){
+            greeting = "Hello, World!";
 
-        // Case 1: No arguments
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-
-            StringBuilder nameBuilder = new StringBuilder();
-
-            // Enhanced for loop
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            // Remove trailing ", "
-            String names = "";
-            if (nameBuilder.length() > 0) {
-                names = nameBuilder.substring(0, nameBuilder.length() - 2);
-            }
-
-            System.out.println("Hello " + names + "!");
+        } else{
+             String names = String.join(", ",args);
+             greeting = "Hello, " + names + "!";
         }
+        System.out.println(greeting);
     }
 }
